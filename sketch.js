08a7fc76410ccf36c4efefd96e7680e3d95e2d76
alinelara091 <162637449+alinelara091@users.noreply.github.com>
2026-01-23 17:33:12 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  
+  background(220);
+  fill(20,20,20);
+  circle(150, 150, 300);
+  fill(255, 255, 255);
+  text("ALINE",60,70)
+ fill(255, 255, 255);
+  text("Audio, Arte sonoro",40,100)
+   fill(255, 255, 255);
+  text("El sentido rítmico del espacio y su relación\n en la escucha periférica",30,120)
+  fill(255, 255, 255);
+  text("Grabación de sonido, edición de sonido,\n producción sonora",20,155)
+}
